60576fd8210f132677bd2587be418baf5adf5ca5
marcomameli <mameli.marco@hotmail.com>
2019-10-29 10:44:18 +0100

    Adding various file for testing different approach

--- LocalBinaryPattern2Version.py
+++ LocalBinaryPattern2Version.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import scipy.ndimage as nd
 import skimage.feature as features
 from skimage.io import imread
+import math
+import os
 #%% Parameter section
 # settings for LBP
 METHOD = 'uniform'
@@ -12,11 +14,9 @@ P = 16
 R = 2
 matplotlib.rcParams['font.size'] = 9
 # Parameters setting
-reference_image_path = 'BaseDocument/Data/templates/ic/ita/CIC/front.png'
-test_image_path = 'FronteCI.jpg'
-# Opening image
-reference_image = imread(reference_image_path, as_gray= True)
-test_image = imread(test_image_path, as_gray= True)
+reference_images_path = 'ReferenceImages'
+test_images_path = 'TestImages/'
+angles = [0, 45, 60, 70, 90, 115, 145, 285]
 
 #%% Function definition
 def kullback_leibler_divergence(p, q):
@@ -25,6 +25,11 @@ def kullback_leibler_divergence(p, q):
     filt = np.logical_and(p != 0, q != 0)
     return np.sum(p[filt] * np.log2(p[filt] / q[filt]))
 
+def bhattacharyya(a, b):
+    """ Bhattacharyya distance between distributions (lists of floats). """
+    if not len(a) == len(b):
+        raise ValueError("a and b must be of the same size")
+    return -math.log(sum((math.sqrt(u * w) for u, w in zip(a, b))))
 
 def match(refs, img):
     best_score = 10
@@ -35,57 +40,53 @@ def match(refs, img):
         ref_hist, _ = np.histogram(ref, density=True, bins=P + 2,
                                    range=(0, P + 2))
         score = kullback_leibler_divergence(hist, ref_hist)
-        if score < best_score:
+        result_hist_comparison = bhattacharyya(ref_hist, hist)
+        if result_hist_comparison < best_score:
             best_score = score
             best_name = name
-    return lbp, best_name, best_score
+    return lbp, best_name, result_hist_comparison
 
 #%% features extraction
-reference_lbp = features.local_binary_pattern(reference_image, P, R, METHOD)
-refs = {
-    'CI': features.local_binary_pattern(reference_image, P, R, METHOD),
-}
+reference_images_lbp = {}
+for root, directories, files in os.walk(reference_images_path):
+    for file in files:
+        if file.endswith('.json') or file.endswith('.DS_Store'):
+            continue
+        else:
+            reference_image_path = os.path.join(root,file)
+            reference_image = imread(reference_image_path, as_gray= True)
+            reference_name = file.split('.')[0]
+            reference_images_lbp[reference_image_path] = features.local_binary_pattern(reference_image, P, R, METHOD)
+#%% Test Extractor and matches
+result_test_images = {}
+for root, directories, files in os.walk(test_images_path):
+    for file in files:
+        test_image_path = os.path.join(root,file)
+        test_image = imread(test_image_path, as_gray= True)
+        test_name = file.split('.')[0]
+        for angle in angles:
+             test_lbp, name, score = match(reference_images_lbp, nd.rotate(test_image, angle=angle, reshape=False))
+             result_test_images[test_image_path + '-' + str(angle)] = {'lbp': test_lbp,
+                                              'angle': angle,
+                                            'name_matched': name,
+                                            'score_matched': score}
 #%% Result
 # classify rotated textures
-print('Result references using LBP:')
-print('Test_image with no rotation')
-test_lbp, name, score = match(refs, test_image)
-print(name, score)
-print('Test_image with 70° rotation')
-test_lbp_rotate70, name_rotate70, score_rotate70 = match(refs, nd.rotate(test_image, angle=70, reshape=False))
-print(name_rotate70, score_rotate70)
-print('Test_image with 145° rotation')
-test_lbp_rotate145, name_rotate145, score_rotate145 = match(refs, nd.rotate(test_image, angle=145, reshape=False))
-print(name_rotate145, score_rotate145)
-print('Test_image with 90° rotation')
-test_lbp_rotate90, name_rotate90, score_rotate90 = match(refs, nd.rotate(test_image, angle=90, reshape=False))
-print(name_rotate90, score_rotate90)
-#%% plotting result
-# plot histograms of LBP of textures
-fig, ((ax1, ax2, ax3, ax4, ax9), (ax5, ax6, ax7, ax8, ax10)) = plt.subplots(nrows=2, ncols=5, figsize=(9, 6))
-plt.gray()
-
-ax1.imshow(reference_image)
-ax1.axis('off')
-ax5.hist(reference_lbp.ravel(), density=True, bins=P + 2, range=(0, P + 2))
-#ax5.set_ylabel('Percentage')
-
-ax2.imshow(test_image)
-ax2.axis('off')
-ax6.hist(test_lbp.ravel(), density=True, bins=P + 2, range=(0, P + 2))
-#ax6.set_ylabel('Percentage')
-
-ax3.imshow(nd.rotate(test_image, angle=70, reshape=False))
-ax3.axis('off')
-ax7.hist(test_lbp_rotate70.ravel(), density=True, bins=P + 2, range=(0, P + 2))
-#ax7.set_xlabel('Uniform LBP values')
-
-ax4.imshow(nd.rotate(test_image, angle=145, reshape=False))
-ax4.axis('off')
-ax8.hist(test_lbp_rotate145.ravel(), density=True, bins=P + 2, range=(0, P + 2))
-
-ax9.imshow(nd.rotate(test_image, angle=90, reshape=False))
-ax9.axis('off')
-ax10.hist(test_lbp_rotate90.ravel(), density=True, bins=P + 2, range=(0, P + 2))
-
-plt.show()
+print('Result of matches using LBP:')
+for name in result_test_images.keys():
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2)
+    plt.gray()
+    result = result_test_images[name]
+    name = name.split('-')
+    test_image = imread(name[0], as_gray= True)
+    reference_image = imread(result['name_matched'], as_gray= True)
+    ax1.imshow(reference_image)
+    ax2.imshow(test_image)
+    ax3.hist(reference_images_lbp[result['name_matched']].ravel(), density=True, bins=P + 2, range=(0, P + 2))
+    ax4.hist(result['lbp'].ravel(), density=True, bins=P + 2, range=(0, P + 2))
+    plt.savefig('ResultMatched/Angle_' + name[1] + '-Test_' + name[0].split('/')[1].split('.')[0] + '-Reference_' +
+                result['name_matched'].split('/')[1].split('.')[0] + '.pdf')
+    plt.show()
+    print('For test image: ' + name[0] + ' with angle: ' + str(result['angle']) +
+          ' the matched reference image is: ' + result['name_matched'] +
+          ' with score: ' + str(result['score_matched']))
